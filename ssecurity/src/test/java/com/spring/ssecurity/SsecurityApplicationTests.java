45afbfbdde1a8dfd9d3a714cb8a427afa369980a
ManidharTamarala <47344401+ManidharTamarala@users.noreply.github.com>
2020-01-17 16:51:08 +0530

    Add files via upload

---
+++ ssecurity/src/test/java/com/spring/ssecurity/SsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.ssecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
